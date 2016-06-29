dd09ce0690b62ba7b7aa50daa9fa37114697739c
Jason Monsorno <JMonsorno@pilotpen.com>
2016-06-29 10:13:47 -0400

    JMonsorno rewriting soft reboot to accomodate videos and new script

--- action.php
+++ action.php
@@ -5,9 +5,12 @@
   switch (strtolower($action)) {
     case 'soft':
     case 'softreboot':
+      exec('killall omxplayer omxplayer.bin videoplayer'); 
       exec('killall -q iceweasel');
-      $url = get_bootSite();
-      exec("(iceweasel $url --display=:0 --profile /home/pi/kioskProfile &) > /dev/null 2>&1");
+      $oldDir = getcwd();
+      chdir('/home/pi/');
+      exec('./setup --silent --nokill 2>&1');
+      chdir($oldDir);
       echo json_encode("done");
       break;
     case 'killvideo':
